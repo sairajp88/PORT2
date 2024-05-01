47406b3ecd945bbe810433fa62fb86ae73dcc1de
sairajp88 <150574772+sairajp88@users.noreply.github.com>
2024-05-02 02:00:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+// server/server.js const express = 
+const express = require('express'); 
+const path = require('path'); 
+const app = express(); 
+require('dotenv').config()
+
+// Serve static files from the 'public' directory 
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Start the server 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => { 
+    console.log(`Server is running on port ${PORT}`); 
+});
